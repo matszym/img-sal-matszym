c0d1e216b32baebdd4ba18a98d57539d359ef99b
Mateusz Szymanski <kontakt@mateuszszymanski.eu>
2016-07-22 21:11:50 +0200

    config loader

--- server.js
+++ server.js
@@ -2,4 +2,8 @@
 
 let express = require('express'),
 app = express(),
+config = require('./config/config.js'),
 server;
+
+server = app.listen(config.port);
+console.log(`Server is listening on port ${config.port}`);
