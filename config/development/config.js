4f165698c4c057ca50ed5bcbb5e78da5e9647199
Mateusz Szymanski <kontakt@mateuszszymanski.eu>
2016-07-23 23:25:15 +0200

    Add flicker api and secret key to development config

--- config/development/config.js
+++ config/development/config.js
@@ -1,4 +1,6 @@
 module.exports = {
   port: 8888,
-  mongo: 'mongodb://localhost/img-sal'
+  mongo: 'mongodb://localhost/img-sal',
+  flickrAPI: process.env.FLICKR_API_KEY,
+  flickrSecret: process.env.FLICKR_SECRET_KEY
 }
