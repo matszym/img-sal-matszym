da23b1ba8389916ae97978b44fd1658f733b433d
Mateusz Szymanski <kontakt@mateuszszymanski.eu>
2016-07-23 23:29:55 +0200

    query route

--- server.js
+++ server.js
@@ -3,7 +3,10 @@
 let express = require('express'),
 app = express(),
 config = require('./config/config.js'),
+imgCtrl = require('./controllers/img.server.controller.js'),
 server;
+
+app.use('/:query', imgCtrl.searchImage);
 
 server = app.listen(config.port);
 console.log(`Server is listening on port ${config.port}`);
