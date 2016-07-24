372fd07fd30bee3635a254277788f6d43b2b88ec
Mateusz Szymanski <kontakt@mateuszszymanski.eu>
2016-07-24 14:46:39 +0200

    add routes to server.js

--- server.js
+++ server.js
@@ -2,11 +2,16 @@
 
 let express = require('express'),
 app = express(),
+mongoose = require('mongoose'),
 config = require('./config/config.js'),
 imgCtrl = require('./controllers/img.server.controller.js'),
-server;
+server,
+db;
 
-app.use('/:query', imgCtrl.searchImage);
+db = mongoose.connect(config.mongo);
+
+app.use('/api/imagesearch/:query', imgCtrl.searchImage, imgCtrl.saveSearch, imgCtrl.getSearch);
+app.use('/api/latest/imagesearch', imgCtrl.getLatest);
 
 server = app.listen(config.port);
 console.log(`Server is listening on port ${config.port}`);
