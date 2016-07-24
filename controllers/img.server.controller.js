77715445af5c653869a7d3597e99128e048b064e
Mateusz Szymanski <kontakt@mateuszszymanski.eu>
2016-07-24 14:48:34 +0200

    create img server controller for routes

--- controllers/img.server.controller.js
+++ controllers/img.server.controller.js
@@ -1,9 +1,54 @@
 'use strict';
 
-let search = require('./search-interface.server.controller.js');
+let search = require('./search-interface.server.controller.js'),
+mongoose = require('mongoose'),
+Promise = require('bluebird');
+
+require('../models/latest.server.model.js');
+
+let Latest = mongoose.model('Latest');
+
+mongoose.Promise = Promise;
 
 exports.searchImage = (req, res, next) => {
-  search().image(req.params.query)
-  .then(result => res.send(result))
-  .catch(err => console.log(err));
+  search().image({
+    text: req.params.query,
+    offset: req.query.offset
+  })
+  .then(result => {
+    req.searchResult = result;
+    next();
+  })
+  .catch(err => res.send(err));
+}
+
+exports.saveSearch = (req, res, next) => {
+  Latest.create({
+    when: Date.now(),
+    term: req.params.query
+  })
+  .then(doc => {
+    next();
+  })
+  .catch(err => res.send(err));
+}
+
+exports.getSearch = (req, res) => res.send(req.searchResult);
+
+exports.getLatest = (req, res) => {
+  Latest.find({})
+  .limit(10)
+  .sort('-when')
+  .then(docs => {
+    let response = docs.map(doc => {
+      return {
+        term: doc.term,
+        when: doc.when
+      };
+    })
+    res.send(response);
+  })
+  .catch(err => {
+    res.send(err);
+  })
 }
